98e263129ab2ca918732499276357dbad6779179
memreyagci <meydeveloper@tutanota.com>
2020-12-02 11:58:29 +0100

    volume module is added.

--- blocks.h
+++ blocks.h
@@ -3,7 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
+	{"", "volume",					                    0,		10},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "battery",					                    5,		3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
