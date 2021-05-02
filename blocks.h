7cf1de344664c7ad02295ba334d77fb7aa769dc2
memreyagci <meydeveloper@tutanota.com>
2021-05-02 18:50:14 +0200

    widget name changes applied.

--- blocks.h
+++ blocks.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		1},
-	{"", "mail",	        	0,  	            2},
-	{"", "newsboat-status",		0,  	            11},
-	{"", "internet",			5,		            4},
-	{"", "volume",				0,		            10},
-	{"", "brightness",			0,		            5},
-	{"", "battery",				5,		            3},
-	{"", "date-time",			5,		            0},
+	/*Icon*/	/*Command*/		    /*Update Interval*/	/*Update Signal*/
+	{"",        "sb-memory",	    10,		            1},
+	{"",        "sb-mail",	        0,  	            2},
+	{"",        "sb-rss",           0,  	            11},
+	{"",        "sb-internet",	    5,		            4},
+	{"",        "sb-volume",	    0,		            10},
+	{"",        "sb-brightness",    0,		            5},
+	{"",        "sb-battery",	    20,		            3},
+	{"",        "sb-date",	        5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
